f837aa3355f5e22dbc8cacae463ff968343244f4
crondolettomata-cpu <crondolettomata@udesa.edu.ar>
2026-06-08 17:29:11 -0300

    guardo cambios para hacer pull

---
+++ demo-expo/src/components/Post.js
@@ -0,0 +1,7 @@
+import {db, auth} from "../firebase/config";
+import firebase from "firebase";
+
+function actualizarDatos(idDelDocumentoAModificar, likes){
+    const emailUsuario = auth.currentUser.email
+    
+}
